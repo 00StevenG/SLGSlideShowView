a0c7479c42e4508bb2860e6f3c331c57b8f9fdb2
works5 <steven@works5.com>
2013-07-13 09:07:34 -0600

    Initial Commit

---
+++ SLGSlideShowView/SLGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SLGAppDelegate.h
+//  SLGSlideShowView
+//
+//  Created by Steven Grace on 7/13/13.
+//  Copyright (c) 2013 Steven Grace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SLGViewController;
+
+@interface SLGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SLGViewController *viewController;
+
+@end
---
+++ SLGSlideShowView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SLGSlideShowView
+//
+//  Created by Steven Grace on 7/13/13.
+//  Copyright (c) 2013 Steven Grace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLGAppDelegate class]));
+    }
+}
